b22696cf63b028083a9bcfda123d1db7c8271c2c
Will Wayman <will.jdw@gmail.com>
2018-12-17 14:31:21 +0000

    method and while loop to test the time taken for four algorithms

---
+++ timing_code.rb
@@ -0,0 +1,47 @@
+require 'benchmark'
+
+def create_array(length)
+  arr = []
+  length.times {arr.push(rand(100))}
+  arr
+end
+
+i = 0000
+while i < 100000 do
+  i += 5000
+  arr = create_array(i)
+  time = Benchmark.realtime do
+    arr.last
+  end
+  puts time*1000
+end
+
+# i = 0000
+# while i < 100000 do
+#   i += 5000
+#   arr = create_array(i)
+#   time = Benchmark.realtime do
+#     arr.reverse
+#   end
+#   puts time*1000
+# end
+#
+# i = 0000
+# while i < 100000 do
+#   i += 5000
+#   arr = create_array(i)
+#   time = Benchmark.realtime do
+#     arr.shuffle
+#   end
+#   puts time*1000
+# end
+#
+# i = 0000
+# while i < 100000 do
+#   i += 5000
+#   arr = create_array(i)
+#   time = Benchmark.realtime do
+#     arr.sort
+#   end
+#   puts time*1000
+# end
