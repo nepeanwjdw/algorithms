d5594c735a6ebef1f9c1fc1a90f852800240f72f
Will Wayman <will.jdw@gmail.com>
2018-12-18 15:24:06 +0000

    algorithm for paring

---
+++ mechacoach_pairing.rb
@@ -0,0 +1,44 @@
+require 'benchmark'
+
+def create_array(length)
+  arr = []
+  alphabet = ("a".."z").to_a
+  length.times do
+    name = ""
+    5.times { name += alphabet[rand(26)] }
+    arr << name
+  end
+  return arr
+end
+
+# attempt 1
+def mechacoach_pairing(arr)
+  arr << "coach" if arr.length.odd?
+  pairs = []
+  no_of_pairs = arr.length/2
+  no_of_pairs.times { pairs << [] }
+  i = 0
+  until arr.empty? do
+    i = 0 if i == no_of_pairs
+    pairs[i] << arr.delete_at(rand(arr.length))
+    i += 1
+  end
+  return pairs
+end
+
+# to test method works
+# p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben", "April"])
+# p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben", "April"])
+# p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben"])
+# p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben"])
+
+# to run tests
+i = 0
+while i < 100000
+  i += 5000
+  arr = create_array(i)
+  time = Benchmark.realtime do
+    mechacoach_pairing(arr)
+  end
+  puts time*1000
+end
