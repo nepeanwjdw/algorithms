f5317427ad2ef649b4844dc9f227804c39d81094
Will Wayman <will.jdw@gmail.com>
2019-01-30 11:22:46 +0000

    refactored

--- mechacoach_pairing.rb
+++ mechacoach_pairing.rb
@@ -16,7 +16,7 @@ def mechacoach_pairing(arr)
   arr << "coach" if arr.length.odd?
   pairs = []
   no_of_pairs = arr.length/2
-  no_of_pairs.times { pairs << [] }
+  no_of_pairs.times { pairs << [] * no_of_pairs }
   i = 0
   until arr.empty? do
     i = 0 if i == no_of_pairs
@@ -27,18 +27,18 @@ def mechacoach_pairing(arr)
 end
 
 # to test method works
-# p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben", "April"])
+p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben", "April"])
 # p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben", "April"])
 # p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben"])
 # p mechacoach_pairing(["John", "Susie", "Clive", "Stacy", "Ben"])
 
-# to run tests
-i = 0
-while i < 100000
-  i += 5000
-  arr = create_array(i)
-  time = Benchmark.realtime do
-    mechacoach_pairing(arr)
-  end
-  puts time*1000
-end
+# # to run tests
+# i = 0
+# while i < 100000
+#   i += 5000
+#   arr = create_array(i)
+#   time = Benchmark.realtime do
+#     mechacoach_pairing(arr)
+#   end
+#   puts time*1000
+# end
